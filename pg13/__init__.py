03a6b0aa0e78d57d5483378d8cbee9faa49e8c4d
Abe Winter <abe-winter@users.noreply.github.com>
2015-02-14 14:38:56 -0500

    removed redismodel from default import because of msgpack dep

--- pg13/__init__.py
+++ pg13/__init__.py
@@ -1,3 +1,4 @@
-import misc,diff,pg,redismodel,syncschema
+import misc,diff,pg,syncschema
 # don't import pgmock and stubredis -- they're only useful for test mode or nonstandard env (i.e. stubredis on windows)
+# don't import redismodel by default -- it has an extra msgpack dependency
 __version__ = '0.0.5'
