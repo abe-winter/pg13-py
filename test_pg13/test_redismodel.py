39f3d2a42e2c6ab3ccd6fb646bd0301567583cc1
Abe Winter <abe-winter@users.noreply.github.com>
2015-05-02 13:12:38 -0400

    [test] redismodel derived class in tests moved to fixture for travis support

--- test_pg13/test_redismodel.py
+++ test_pg13/test_redismodel.py
@@ -12,13 +12,16 @@ pytestmark = pytest.mark.travis_skip
 # ugly: pie-in-the-sky guess to prevent interfering with prod redis data on my laptop
 TEST_DB = 6
 
-class RM(redismodel.RedisModel):
-  NAMESPACE = 'test_redismodel_RM'
-  VERSION = 0
-  KEY = [('a',int),('b',int)]
-  VALUE = [('c',basestring),('d',list)]
+@pytest.fixture
+def RM():
+  class RM(redismodel.RedisModel):
+    NAMESPACE = 'test_redismodel_RM'
+    VERSION = 0
+    KEY = [('a',int),('b',int)]
+    VALUE = [('c',basestring),('d',list)]
+  return RM
 
-def test_getitem():
+def test_getitem(RM):
   rm = RM([0,1], ['a',[]])
   assert rm['a'] == 0
   assert rm['b'] == 1
@@ -27,7 +30,7 @@ def test_getitem():
 
 @pytest.mark.integration
 @pytest.mark.redis
-def test_setget():
+def test_setget(RM):
   con = redis.Redis(db=TEST_DB)
   rm = RM([0,1], ['a',[]])
   rm.save(con)
